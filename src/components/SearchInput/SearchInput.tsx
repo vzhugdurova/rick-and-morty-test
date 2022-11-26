7fbd910809d2c1c74ddebd1a982f941e6456c733
Vika <zhuuvika@gmail.com>
2022-11-26 20:36:44 +0300

    added search character input

---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,9 @@
+import {OutlinedInput} from '@mui/material';
+
+const SearchInput = () => {
+  return (
+    <OutlinedInput style={{borderRadius: 0, width: "100%"}}/>
+  )
+}
+
+export default SearchInput
